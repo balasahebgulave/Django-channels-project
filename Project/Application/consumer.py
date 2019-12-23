c082615867ce185ae24db14fcf944c8521bb4a4f
balasaheb <shreepad.khandve@sitpune.edu.in>
2019-12-23 15:53:28 +0530

    second commit

--- Project/Application/consumer.py
+++ Project/Application/consumer.py
@@ -1,5 +1,6 @@
 import asyncio
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.consumer import AsyncConsumer
 
 
 class TickTockConsumer(AsyncJsonWebsocketConsumer):
@@ -11,3 +12,29 @@ class TickTockConsumer(AsyncJsonWebsocketConsumer):
             await self.send_json("tick")
             await asyncio.sleep(1)
             await self.send_json(".....tock")
+
+
+class ChatConsumer(AsyncConsumer):
+	async def websocket_connect(self, event):
+		print('------------connected---------',event)
+		await self.send({
+			"type":"websocket.accept"
+		})
+
+		await asyncio.sleep(2)
+
+		await self.send({
+			"type":"websocket.send",
+			"text":"Hello World !"
+		})
+
+		# await self.send({
+		# 	"type":"websocket.close",
+		# })
+
+	async def websocket_receive(self, event):
+		print('------------received---------',event)
+
+	async def websocket_disconnect(self, event):
+		print('------------disconnected---------',event)
+
--- Project/Project/routing.py
+++ Project/Project/routing.py
@@ -1,10 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from Application.consumer import TickTockConsumer
+from Application.consumer import TickTockConsumer, ChatConsumer
 from django.urls import path
 
 urlpatterns = ProtocolTypeRouter({
 	"websocket":URLRouter([
 	path('ws/',TickTockConsumer),
+	path('chat/',ChatConsumer)
 
 	])
 })
